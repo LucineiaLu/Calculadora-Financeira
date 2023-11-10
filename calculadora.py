4d6f4eae738a08709172369b8ee3612abc5ef839
LucineiaLu <149438779+LucineiaLu@users.noreply.github.com>
2023-11-10 16:14:24 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,23 +1,4 @@
 
-# Projeto 1
-# Neste projeto, os alunos criarão uma calculadora financeira em Python que ajudará os
-# usuários a calcular empréstimos com base em sua renda. A aplicação incluirá as seguintes
-# etapas:
-# ➔ Validação de Renda:
-# ◆ Os usuários inserirão informações sobre sua renda mensal.
-# ◆ A aplicação verificará se a renda inserida é válida e está dentro de um limite
-# específico.
-# ➔ Cálculo de Empréstimo:
-# ◆ Após a validação bem-sucedida da renda, os usuários poderão inserir o valor
-# do empréstimo desejado e o prazo.
-# ◆ A aplicação calculará a taxa de juros apropriada e o valor das prestações
-# mensais.
-# ➔ Apresentação dos Resultados:
-# ◆ A aplicação exibirá os resultados do cálculo, incluindo o valor das prestações
-# mensais e o custo total do empréstimo.
-
-
-
 # Função para validar a renda do usuário
 def validar_renda(renda):
     limite_minimo = 1000  # Defina o limite mínimo de renda desejado
